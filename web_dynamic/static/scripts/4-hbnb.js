b4ef0ef69ec616a2f6cec0f19c2f6a31691cf952
Abdurlahman Khaled <21-101079@students.eui.edu.eg>
2024-03-04 03:02:25 +0200

    Added task '5. Filter places by Amenity'
    Replace the route 3-hbnb with 4-hbnb in the file 4-hbnb.py (based on 3-hbnb.py)
    
    Create a new template 4-hbnb.html (based on 3-hbnb.html) and update it:
    - Import the JavaScript static/scripts/4-hbnb.js in the <head> tag (instead of 3-hbnb.js)
    
    Write a JavaScript script (static/scripts/4-hbnb.js):
    - Based on 3-hbnb.js
    - When the button tag is clicked, a new POST request to places_search should be made with the list of Amenities checked
    
    Now you have the first filter implemented, enjoy!
    
    Files:
    - 4-hbnb.py
    - templates/4-hbnb.html
    - static/scripts/4-hbnb.js

---
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -0,0 +1,126 @@
+$(document).ready(function () {
+  const dict = {};
+  const $amenitiesCheck = $('input[type=checkbox]');
+  const $selectedAmenities = $('div.amenities h4');
+  const $filterPlacesButton = $('section.filters button');
+  const $statusIndicator = $('div#api_status');
+  const statusUri = 'http://localhost:5001/api/v1/status/';
+  const placesUri = 'http://localhost:5001/api/v1/places_search/';
+  const $placesSection = $('section.places');
+
+  function createMaxGuests (guests) {
+    if (guests !== 1) {
+      return `<div class="max_guest">${guests} Guests</div>`;
+    }
+
+    return `<div class="max_guest">${guests} Guest</div>`;
+  }
+
+  function createRooms (rooms) {
+    if (rooms !== 1) {
+      return `<div class="number_rooms">${rooms} Bedrooms</div>`;
+    }
+
+    return `<div class="number_rooms">${rooms} Bedroom</div>`;
+  }
+
+  function createBathrooms (bathrooms) {
+    if (bathrooms !== 1) {
+      return `<div class="number_bathrooms">${bathrooms} Bathrooms</div>`;
+    }
+
+    return `<div class="number_bathrooms">${bathrooms} Bathroom</div>`;
+  }
+
+  function createArticleTitle (title, price) {
+    return `<div class="title_box">
+              <h2>${title}</h2>
+              <div class="price_by_night">
+                $${price}
+              </div>
+            </div>`;
+  }
+
+  function createArticleInfo (guests, rooms, bathrooms) {
+    const maxGuests = createMaxGuests(guests);
+    const maxRooms = createRooms(rooms);
+    const maxBathrooms = createBathrooms(bathrooms);
+    return `<div class="information">
+              ${maxGuests}
+              ${maxRooms}
+              ${maxBathrooms}
+            </div>`;
+  }
+
+  function createArticleDescription (description) {
+    return `<div class="description">
+              ${description}
+            </div>`;
+  }
+
+  function createPlace (place) {
+    const articleTitle = createArticleTitle(place.name, place.price_by_night);
+    const articleInfo = createArticleInfo(place.max_guest, place.number_rooms, place.number_bathrooms);
+    const articleDescription = createArticleDescription(place.description);
+    return `<article>
+              ${articleTitle}
+              ${articleInfo}
+              ${articleDescription}
+            </article>`;
+  }
+
+  function fetchPlaces (amenities) {
+    return $.ajax({
+      url: placesUri,
+      type: 'POST',
+      dataType: 'json',
+      data: JSON.stringify(amenities),
+      contentType: 'application/json',
+      done: function (data) {
+        return data;
+      }
+    });
+  }
+
+  async function refreshHtmlPlaces (amenities = {}) {
+    const places = await fetchPlaces(amenities);
+
+    for (let i = 0; i < places.length; ++i) {
+      $placesSection.append(createPlace(places[i]));
+    }
+  }
+
+  $filterPlacesButton.on('click', function () {
+    const amenitiesIds = {
+      amenities: Object.keys(dict)
+    };
+
+    $placesSection.text('');
+    refreshHtmlPlaces(amenitiesIds);
+  });
+
+  $amenitiesCheck.click(function () {
+    if ($(this).is(':checked')) {
+      dict[$(this).data('id')] = $(this).data('name');
+      $selectedAmenities.text(Object.values(dict).join(', '));
+    } else if ($(this).is(':not(:checked)')) {
+      delete dict[$(this).data('id')];
+      $selectedAmenities.text(Object.values(dict).join(', '));
+    }
+  });
+
+  $.ajax({
+    url: statusUri,
+    type: 'GET',
+    dataType: 'json',
+    success: function (data) {
+      if (data.status === 'OK') {
+        $statusIndicator.addClass('available');
+      } else {
+        $statusIndicator.removeClass('available');
+      }
+    }
+  });
+
+  refreshHtmlPlaces();
+});
